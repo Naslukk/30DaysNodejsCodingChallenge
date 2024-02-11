7a392ab01e735a8f7c957695dc43baa3ef9f0bda
Naslu kk <naslukk@gmail.com>
2024-02-11 09:59:07 +0530

    Day 7 updated

--- day7/code.js
+++ day7/code.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+function requestLoggerMiddleware(req, res, next) {
+    const timestamp = new Date().toISOString();
+    const method = req.method;
+    console.log(`${timestamp} - ${method} request received.`);
+    next();
+}
+
+
+app.use(requestLoggerMiddleware);
+
+
+app.get('/', requestLoggerMiddleware);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-app.get('/greet', (req, res) => {
-    let name = req.query.params;
-
-    if (name){
-        res.send(`Hello, ${name}`);
-        console.log(`Hello, ${name}`);
-    }else{
-        res.send('Hello ,Guest!');
-    }
-})
-
-    app.listen(3030,()=>{
-        console.log('listening on port 3030');
-    })
